814380e64870127b66a03d7c441494fe93d101f2
Hans Desmet <hans.desmet@vdabcampus.be>
2022-07-07 07:31:52 +0200

    testen uitvoeren

--- src/test/java/org/example/MyTest.java
+++ src/test/java/org/example/MyTest.java
@@ -3,9 +3,11 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class MyTest {
     @Test
     void test() {
-        Assertions.assertEquals(2, 2);
+        assertThat(2).isEqualTo(2);
     }
 }
