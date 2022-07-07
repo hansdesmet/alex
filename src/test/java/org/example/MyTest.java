a03775d4cea244470407db1e6ef6e9756328a630
Hans Desmet <hans.desmet@vdabcampus.be>
2022-07-07 07:06:52 +0200

    initial commit

---
+++ src/test/java/org/example/MyTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    void test() {
+        Assertions.assertEquals(2, 2);
+    }
+}
